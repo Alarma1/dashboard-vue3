42cc8cc8e17db2efaada5d60f934b189e4f5a582
Alarma1 <zapara03@yandex.ru>
2024-02-08 05:19:16 +0500

    внес исправления

--- src/components/paymentStatus.vue
+++ src/components/paymentStatus.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[84px] h-[22px] py-0.5 px-2.5 text-xs font-normal rounded-full bg-green-100 flex items-center align-center  text-green-800 text-xs font-normal"
+    <div class="w-[84px] h-[22px] py-0.5 px-2.5 text-xs font-normal rounded-full flex justify-center items-center align-center text-xs font-normal"
          :class="{
         'bg-green-100': status === 'Completed','text-green-800': status === 'Completed',
          'bg-blue-100': status === 'In progress','text-blue-800': status === 'In progress',
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ErrorPage from "../views/ErrorPage.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'error',
-      component: ErrorPage
-    },
-  ]
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'error',
+            component: ErrorPage
+        },
+    ]
 })
 
 export default router
